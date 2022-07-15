60191c10baf853582ba4c868b60e1513d449e680
Daniel Conlon <danielcconlon@gmail.com>
2022-07-15 15:51:59 -0400

    setup the models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,46 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validate that the value is a decimal
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // set value to 10
+      defaultValue: 10,
+      // validates that the value is numeric
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      // references the category model's id
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
